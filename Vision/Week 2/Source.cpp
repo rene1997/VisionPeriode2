124f8d027dc431f707c5dcc2e4b6d4564aae0ef0
rene <renekeijzer1997@gmail.com>
2017-11-15 11:09:37 +0100

    project init

---
+++ Vision/Week 2/Source.cpp
@@ -0,0 +1,18 @@
+#include <opencv2\opencv.hpp>
+#include <iostream>
+
+using namespace std;
+
+int runOpdracht2();
+
+int main() {
+	runOpdracht2();
+}
+
+
+int runOpdracht2() {
+
+
+
+	return 0;
+}
